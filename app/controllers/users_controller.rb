c0b04b0a50f87e25f6d2cdf8a5fca8b6b89b788d
sho.eda <shoeda.works@gmail.com>
2021-02-11 03:49:16 +0000

    login_create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show, :edit]
   
   
   def show
-    @user = User.find(params[:id])
+    set_user
   end
 
   def new
