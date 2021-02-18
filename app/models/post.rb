815cdcb55ab811f7482ea6ad094f14c8e6da7124
sho.eda <shoeda.works@gmail.com>
2021-02-18 02:54:33 +0000

    nav_style

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
     validates :content, presence: false, length: { maximum: 255 }
+    # validates :images, blob: { content_type: %w(image/png image/jpg image/jpeg) }
     
     # 画像サイズ
     # image_tag @post.image.variant(resize_to_fill: [128, 128])
